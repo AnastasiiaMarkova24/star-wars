94395e81f9420b1a9d9ae4f32e7c358984303fdb
DESKTOP-QDQJVSE\Private_567 <markova2415@gmail.com>
2020-12-17 23:00:35 +0200

    formatted

--- src/constants/films.ts
+++ src/constants/films.ts
@@ -4,7 +4,7 @@ export const FilmsActionTypes: IFilmsActionTypes = {
     setIsLoading: 'SET_IS_LOADING',
     setFilms: 'SET_FILMS',
     markAsFavourite: 'MARK_AS_FAVOURITE'
-}
+};
 
 export const FavouriteFilms = 'FAVOURITE_FILMS';
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
 import Home from './components/home';
 import { Provider } from 'react-redux';
 import { store } from './store';
